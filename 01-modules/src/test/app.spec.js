a6bdec361c5f56957952d2b3ee99837b5e1bd0eb
JPeer264 <jan.oster94@gmail.com>
2016-11-06 08:58:34 +0100

    updated tests

--- 01-modules/src/test/app.spec.js
+++ 01-modules/src/test/app.spec.js
@@ -5,18 +5,18 @@ describe('app.js', () => {
     describe('valid', () => {
         it('should have a valid university email', done => {
             const validemail  = 'jstoecklmair.mmt-m@fh-salzburg.ac.at';
-            const validemail2 = 'fhs39850@fh-salzburg.ac.at';
 
             expect(app.valid(validemail)).to.be.true;
-            expect(app.valid(validemail2)).to.be.true;
         });
 
         it('should fail', done => {
             const nonvalieemail  = 'janpeer264@gmail.com';
             const nonvalieemail2 = 'goofy@example.com';
+            const nonvalieemail3 = 'fhs39850@fh-salzburg.ac.at';
 
             expect(app.valid(nonvalieemail)).to.be.false;
             expect(app.valid(nonvalieemail2)).to.be.false;
+            expect(app.valid(nonvalieemail3)).to.be.false;
 
             done();
         });
@@ -27,8 +27,8 @@ describe('app.js', () => {
             const validemail  = 'jstoecklmair.mmt-m2016@fh-salzburg.ac.at';
             const validemail2 = 'jstoecklmair.heb-b2016@fh-salzburg.ac.at';
 
-            expect(app.degreeProgramm(validemail)).to.equal('multimediatechnology');
-            expect(app.degreeProgramm(validemail2)).to.equal('hebame');
+            expect(app.degreeProgramm(validemail)).to.equal('MMT');
+            expect(app.degreeProgramm(validemail2)).to.equal('HEB');
 
             done();
         });
