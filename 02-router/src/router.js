85aacac4a9e7d95e7fedc9086950231e50c2a7cb
JPeer264 <jan.oster94@gmail.com>
2016-11-09 16:46:11 +0100

    added some regex

--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,3 +1,25 @@
-export default function() {
+let registeredRoutes = [];
 
+export default function(path, template) {
+    // register paths
+    registeredRoutes.push(path);
+
+    /**
+     * ✅/
+     * ✅/contact
+     * ❓/contact/:parameter
+     */
+
+    const getParameter = /(:[\w\d]+)\//g;
+
+
+    if (path === window.location.pathname && registeredRoutes.includes(path)) {
+        if (typeof template === 'function' && template !== undefined) {
+            template();
+        }
+    } else if (path === '*' && !registeredRoutes.includes(window.location.pathname)) {
+        if (typeof template === 'function' && template !== undefined) {
+            template();
+        }
+    }
 }
